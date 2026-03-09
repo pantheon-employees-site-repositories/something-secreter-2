0dcf018307500ada29903b02398fe021296a2827
Greg Anderson <greg@pantheon.io>
2026-03-08 20:14:40 -0700

    Add an external test library

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+import { sample_node_library_test } from "sample-node-library";
+
 export default function Home() {
+  useEffect(() => {
+    sample_node_library_test();
+  }, []);
+
   return <div>Welcome to Pantheon Platform</div>;
 }
